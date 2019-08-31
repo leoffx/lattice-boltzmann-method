0e4bd10dcf9be6bc7d0083551fa6d44a8b262032
leoffx <43040272+leoffx@users.noreply.github.com>
2019-08-31 15:43:09 -0300

    Create porous-d2q9.py

---
+++ porous-d2q9.py
@@ -0,0 +1,162 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation
+
+# DEFINIR:
+height = 128
+width = 200
+viscosity = 0.01  # viscosidade // proporcional ao inverso de reynolds // não diminuir muito
+omega = 1 / (3 * viscosity + 0.5)  # parametro de relaxamento
+
+
+
+u0 = 0.08  # velocidade inicial // não aumentar muito
+
+u = np.zeros((2, height, width))
+fin = np.zeros((9, height, width))
+
+# inicialização da densidade, dado u0
+fin[0, :, :] = 4. / 9. * (1 - 1.5 * u0**2)
+fin[2, :, :] = 1. / 9. * (1 - 1.5 * u0**2)
+fin[4, :, :] = 1. / 9. * (1 - 1.5 * u0**2)
+fin[1, :, :] = 1. / 9. * (1 + 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+fin[3, :, :] = 1. / 9. * (1 - 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+fin[5, :, :] = 1. / 36. * (1 + 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+fin[8, :, :] = 1. / 36. * (1 + 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+fin[6, :, :] = 1. / 36. * (1 - 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+fin[7, :, :] = 1. / 36. * (1 - 3 * u0 + 4.5 * u0**2 - 1.5 * u0**2)
+rho = np.sum(fin, axis=0)  # densidade macroscopica
+
+fin1 = fin[1, 0, 0]
+fin5 = fin[5, 0, 0]
+fin8 = fin[8, 0, 0]
+
+fin6 = fin[6, 0, 0]
+fin7 = fin[7, 0, 0]
+fin3 = fin[3, 0, 0]
+
+# obstáculo / objeto
+obj = np.zeros((height, width), bool)
+#criar circulo:
+for k in range(11):
+    for i in range(height):
+        for j in range(width):
+            if (i - 10 - 15* k )**2 + (j - 50)**2 < 10:
+                obj[i, j] = True
+
+obj[1, :] = True
+obj[-2, :] = True
+objN = np.roll(obj, 1, axis=0)
+objS = np.roll(obj, -1, axis=0)
+objE = np.roll(obj, 1, axis=1)
+objW = np.roll(obj, -1, axis=1)
+objNE = np.roll(objN, 1, axis=1)
+objNW = np.roll(objN, -1, axis=1)
+objSE = np.roll(objS, 1, axis=1)
+objSW = np.roll(objS, -1, axis=1)
+
+
+# streaming step
+def streaming():
+    global fin
+    fin[2, :, :] = np.roll(fin[2, :, :], 1, axis=0)
+    fin[5, :, :] = np.roll(fin[5, :, :], 1, axis=0)
+    fin[6, :, :] = np.roll(fin[6, :, :], 1, axis=0)
+
+    fin[4, :, :] = np.roll(fin[4, :, :], -1, axis=0)
+    fin[8, :, :] = np.roll(fin[8, :, :], -1, axis=0)
+    fin[7, :, :] = np.roll(fin[7, :, :], -1, axis=0)
+
+    fin[1, :, :] = np.roll(fin[1, :, :], 1, axis=1)
+    fin[5, :, :] = np.roll(fin[5, :, :], 1, axis=1)
+    fin[8, :, :] = np.roll(fin[8, :, :], 1, axis=1)
+
+    fin[3, :, :] = np.roll(fin[3, :, :], -1, axis=1)
+    fin[6, :, :] = np.roll(fin[6, :, :], -1, axis=1)
+    fin[7, :, :] = np.roll(fin[7, :, :], -1, axis=1)
+
+    # condiçao de contorno no objeto
+    fin[2, :, :][objN] = fin[4, :, :][obj]
+    fin[4, :, :][objS] = fin[2, :, :][obj]
+
+    fin[1, :, :][objE] = fin[3, :, :][obj]
+    fin[3, :, :][objW] = fin[1, :, :][obj]
+
+    fin[5, :, :][objNE] = fin[7, :, :][obj]
+    fin[6, :, :][objNW] = fin[8, :, :][obj]
+
+    fin[8, :, :][objSE] = fin[6, :, :][obj]
+    fin[7, :, :][objSW] = fin[5, :, :][obj]
+
+
+# collision step
+def collision():
+    global fin
+    rho = np.sum(fin, axis=0)
+    u[0, :, :] = (fin[1, :, :] + fin[5, :, :] + fin[8, :, :] - fin[3, :, :] -
+                  fin[6, :, :] - fin[7, :, :]) / rho
+    u[1, :, :] = (fin[2, :, :] + fin[5, :, :] + fin[6, :, :] - fin[4, :, :] -
+                  fin[8, :, :] - fin[7, :, :]) / rho
+
+    u2 = u[0, :, :]**2 + u[1, :, :]**2
+    uxuy = u[0, :, :] * u[1, :, :]
+    um32u2 = 1 - 1.5 * (u2)  #um menos 3/2 de u**2
+
+    fin[0, :, :] = fin[0, :, :] * (1. - omega) + omega * 4. / 9. * rho * (
+        um32u2)
+    fin[1, :, :] = fin[1, :, :] * (1. - omega) + omega * 1. / 9. * rho * (
+        3. * u[0, :, :] + 4.5 * u[0, :, :]**2 + um32u2)
+    fin[2, :, :] = fin[2, :, :] * (1. - omega) + omega * 1. / 9. * rho * (
+        3. * u[1, :, :] + 4.5 * u[1, :, :]**2 + um32u2)
+    fin[3, :, :] = fin[3, :, :] * (1. - omega) + omega * 1. / 9. * rho * (
+        -3. * u[0, :, :] + 4.5 * u[0, :, :]**2 + um32u2)
+    fin[4, :, :] = fin[4, :, :] * (1. - omega) + omega * 1. / 9. * rho * (
+        -3. * u[1, :, :] + 4.5 * u[1, :, :]**2 + um32u2)
+    fin[5, :, :] = fin[5, :, :] * (1. - omega) + omega * 1. / 36. * rho * (
+        3. * (u[0, :, :] + u[1, :, :]) + 4.5 * (u2 + 2. * uxuy) + um32u2)
+    fin[6, :, :] = fin[6, :, :] * (1. - omega) + omega * 1. / 36. * rho * (
+        3. * (-u[0, :, :] + u[1, :, :]) + 4.5 * (u2 - 2. * uxuy) + um32u2)
+    fin[7, :, :] = fin[7, :, :] * (1. - omega) + omega * 1. / 36. * rho * (
+        3. * (-u[0, :, :] - u[1, :, :]) + 4.5 * (u2 + 2. * uxuy) + um32u2)
+    fin[8, :, :] = fin[8, :, :] * (1. - omega) + omega * 1. / 36. * rho * (
+        3. * (u[0, :, :] - u[1, :, :]) + 4.5 * (u2 - 2. * uxuy) + um32u2)
+
+    # fluxo no inicio do quadro
+    fin[1, :, :][:, 0] = fin1
+    fin[3, :, :][:, 0] = fin3
+    fin[5, :, :][:, 0] = fin5
+    fin[8, :, :][:, 0] = fin8
+    fin[6, :, :][:, 0] = fin6
+    fin[7, :, :][:, 0] = fin7
+
+
+u_plot = np.abs(u[0, :, :], u[1, :, :])
+
+# PLOT LOOP
+theFig = plt.figure(figsize=(8, 3))
+
+bImageArray = np.zeros((height, width, 4), np.uint8)
+bImageArray[obj, 3] = 255
+objImage = plt.imshow(bImageArray)
+
+def curl(ux, uy):
+	return np.roll(uy,-1,axis=1) - np.roll(uy,1,axis=1) - np.roll(ux,-1,axis=0) + np.roll(ux,1,axis=0)
+
+
+def nextStep(arg):
+    global rho
+    for _ in range(20):  #quantos calculos vão ser feitos por passo
+        streaming()
+        collision()
+
+    #u_plot = np.abs(u[0, :, :], u[1, :, :])
+    #fluidImage = plt.imshow(u_plot)
+
+    fluidImage = plt.imshow(curl(u[0, :, :], u[1, :, :]))
+
+    return (fluidImage, objImage)
+
+
+animate = matplotlib.animation.FuncAnimation(
+    theFig, nextStep, interval=1, blit=True)
+plt.show()
